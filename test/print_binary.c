e068b0efb06d449469a0d1c3b8b88cac9d7c2115
mess_web <meszewde@gmail.com>
2022-04-19 07:22:14 -0700

    tihis the first task

--- test/print_binary.c
+++ test/print_binary.c
@@ -1,54 +1,16 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_binary - converts an unsigned int to base2 and inserts into string
- * @format: the string to be modified
- * @var: the va_list to be accessed
- * Return: pointer to the modified string
+ * print_binary - function that prints the binary representation of a number
+ * @n: number to be printed in binary
+ * @printed: hold the number of characters printed
  */
-
-char *print_binary(char *format, va_list *var)
+void print_binary(unsigned int n, unsigned int *printed)
 {
-	char *formatEnd, newStr[32];
-	unsigned int arg, i, idx;
-	int argTest = 0;
-
-	arg = va_arg(*var, unsigned int);
-	argTest = arg;
-	if (argTest < 0)
-		exit(101);
-
-	formatEnd = malloc(sizeof(*formatEnd) * (_strlen(format) + 1));
-	if (formatEnd == NULL)
-		return (NULL);
-	_strcpy(formatEnd, format);
-
-	idx = 0;
-	while (arg / 2 != 0)
+	if (n > 1)
 	{
-		newStr[idx] = (arg % 2) + 48;
-		arg = arg / 2;
-		idx++;
+		*printed += 1;
+		print_binary(n >> 1, printed);
 	}
-	newStr[idx] = (arg % 2) + 48;
-
-	i = 0;
-	while (idx > 0)
-	{
-		format[i] = newStr[idx];
-		i++;
-		idx--;
-	}
-	format[i] = newStr[idx];
-
-	format = format + i + 1;
-
-	if (_strlen(formatEnd) > 2)
-		_strcpy(format, formatEnd + 2);
-	else
-		*format = '\0';
-
-	return (format);
+	_putchar((n & 1) + '0');
 }
